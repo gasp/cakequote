f10df93fb7ee9e0ec317e468a1b9ee6125c2d465
gaspard <gaspard@gmail.com>
2013-02-19 15:24:30 +0100

    dump: adding sample data

--- app/Config/Schema/cakequote.sql
+++ app/Config/Schema/cakequote.sql
@@ -4,7 +4,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.1.44-log)
 # Database: cakequote
-# Generation Time: 2013-02-19 13:39:35 +0000
+# Generation Time: 2013-02-19 14:23:39 +0000
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -47,19 +47,26 @@ DROP TABLE IF EXISTS `quotes`;
 
 CREATE TABLE `quotes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slug` char(15) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `body` text,
+  `user_id` int(10) unsigned DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `updated` (`updated`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `quotes` WRITE;
 /*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
-INSERT INTO `quotes` (`id`,`title`,`body`,`created`,`updated`)
+INSERT INTO `quotes` (`id`,`slug`,`title`,`body`,`user_id`,`created`,`updated`)
 VALUES
-	(1,'hasta la vista','Hasta la vista baby','2013-02-19 14:06:17',NULL);
+	(1,'hasta','hasta la vista','Hasta la vista baby',1,'2013-02-19 14:06:17',NULL),
+	(2,'carpe-diem','carpe diem','Carpe Diem',1,'2013-02-19 15:19:09',NULL),
+	(3,'wtf','what the f','What the F*ck',2,'2013-02-19 15:21:22',NULL),
+	(4,'mieux-vaut','mieux vaut','Mieux vaut etre seul que mal accompagne',2,'2013-02-19 15:21:18',NULL),
+	(5,'i-am-your-fathe','i am your father','I am your Father',2,'2013-02-19 15:21:58',NULL),
+	(6,'es-ist-nicht-al','es ist nicht alles gold was glanzt','Es ist nicht alles gold was glanzt',2,'2013-02-19 15:23:30',NULL);
 
 /*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -79,13 +86,16 @@ CREATE TABLE `users` (
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`,`username`,`password`,`email`,`group_id`,`created`,`updated`)
 VALUES
-	(1,'admin','pass','gasp@altern.org',1,'2013-02-19 13:42:43',NULL);
+	(1,'admin','pass','admin@admin.com',1,'2013-02-19 13:42:43',NULL),
+	(2,'bob','pass','bob@bob.com',2,'2013-02-19 15:19:59',NULL),
+	(3,'alice','pass','alice@alice.com',3,'2013-02-19 15:20:16',NULL),
+	(4,'charlie','pass','charlie@charlie.com',3,'2013-02-19 15:20:32',NULL);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
